1221b95830301e1ae7b2cf5474edd0d7cc580c4e
SimonM <podvilla@gmail.com>
2016-10-30 17:53:09 +0000

    Renamed source folder to basic

--- source.asm
+++ source.asm
@@ -5,22 +5,22 @@
 ;----------------------------------------------------------------------------------------------------------
 
 ; Create a disk of various BASIC source file demonstrations
-PUTBASIC "source\3D.txt", "3D"
-PUTBASIC "source\3DSolid.txt", "3DSolid"
-PUTBASIC "source\Polyhed.txt", "Polyhed"
-PUTBASIC "source\Menu.txt", "Menu"
-PUTBASIC "source\Filler.txt", "Filler"
-PUTBASIC "source\Line5.txt", "S.Line5"
-PUTBASIC "source\Line3D.txt", "S.Line3D"
+PUTBASIC "basic\3D.txt", "3D"
+PUTBASIC "basic\3DSolid.txt", "3DSolid"
+PUTBASIC "basic\Polyhed.txt", "Polyhed"
+PUTBASIC "basic\Menu.txt", "Menu"
+PUTBASIC "basic\Filler.txt", "Filler"
+PUTBASIC "basic\Line5.txt", "S.Line5"
+PUTBASIC "basic\Line3D.txt", "S.Line3D"
 
 ; Compiled object files for line drawing
-PUTFILE "source\Line5.o", "Line5", &2F03
-PUTFILE "source\Line3D.o", "Line3D", &26BA
+PUTFILE "basic\Line5.o", "Line5", &2F03
+PUTFILE "basic\Line3D.o", "Line3D", &26BA
 
 PUTFILE "README.md", "README", 0
 
 ; Add the boot file
-PUTFILE "source\!Boot.txt", "!Boot", 0
+PUTFILE "basic\!Boot.txt", "!Boot", 0
 
 ;SAVE "!Boot", start, end, entry
 
